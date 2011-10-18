2ea41fcae79d9a412008646d1fc0caaacbf13ae3
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-10-17 21:28:24 -0400

    added category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111018012241) do
+ActiveRecord::Schema.define(:version => 20111018012715) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
