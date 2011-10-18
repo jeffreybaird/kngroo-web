3d87413fef0cd99fc626d08e186e29213d18d460
Aubrey Goodman <aubrey.goodman@gmail.com>
2011-10-17 21:26:23 -0400

    initial commit with basic user model and no tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kngroo::Application.config.secret_token = '1a88e19965a6f545f6951e7da7d1ab14223c7fcfc4d94c587612c2b20173f3b92d56109b7b3244b3176d88fbc73f03aadb43eae3d886c7dcdf0d8048f6912a5f'
