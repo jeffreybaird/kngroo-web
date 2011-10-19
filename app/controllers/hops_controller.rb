830fe9387bda96fcef9c75e28188b5f950c29cb9
Aubrey <aubrey.goodman@gmail.com>
2011-10-19 17:13:26 -0400

    prune user hops from full set when signed in

--- app/controllers/hops_controller.rb
+++ app/controllers/hops_controller.rb
@@ -3,7 +3,12 @@ class HopsController < ApplicationController
   before_filter :authorize
   
   def index
-    @hops = Hop.all
+    if signed_in?
+      @hops = Hop.all - current_user.hops
+    else
+      @hops = Hop.all
+    end
+
     respond_to do |format|
       format.html
       format.json { render :json => @hops }
