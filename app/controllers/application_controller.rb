6b27ea2d79395cac2bb78031cd2055c86b4eadb8
Aubrey <aubrey.goodman@gmail.com>
2011-10-30 17:18:51 -0400

    added app-level not found and routing error catch-alls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   include Clearance::Authentication
   protect_from_forgery
+
+  rescue_from(ActiveRecord::RecordNotFound) do |e| 
+    respond_to do |format| 
+      format.html { render :file => 'public/404.html' } 
+      format.json { render :json => { :errors => [ "Record does not exist"] }, :status => :not_found }
+    end
+  end
+  rescue_from(ActionController::RoutingError) do |e|
+    respond_to do |format| 
+      format.html { render :file => 'public/404.html' } 
+      format.json { render :json => { :errors => [ "Record does not exist"] }, :status => :not_found }
+    end
+  end
+  
 end
